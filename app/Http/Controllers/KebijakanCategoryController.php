e37e999325de62bab259832af6b09f71d03fa1d3
bintoroadhi <bintoroadhisetiawan@gmail.com>
2022-11-11 08:09:04 +0700

    migrasi categories

---
+++ app/Http/Controllers/KebijakanCategoryController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\KebijakanCategory;
+use Illuminate\Http\Request;
+
+class KebijakanCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\KebijakanCategory  $kebijakanCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function show(KebijakanCategory $kebijakanCategory)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\KebijakanCategory  $kebijakanCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(KebijakanCategory $kebijakanCategory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\KebijakanCategory  $kebijakanCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, KebijakanCategory $kebijakanCategory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\KebijakanCategory  $kebijakanCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(KebijakanCategory $kebijakanCategory)
+    {
+        //
+    }
+}
