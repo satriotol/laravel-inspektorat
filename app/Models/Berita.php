28f876f6279eaf50c545543264dcfb2ca68f43f7
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2022-11-11 12:51:07 +0700

    berita

--- app/Models/Berita.php
+++ app/Models/Berita.php
@@ -8,6 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Berita extends Model
 {
     use HasFactory;
-    protected $fillable=['user_id', 'verified_by', 'berita_category_id', 'berita_subcategory_id', 'title', 'description', 'image', 'is_verified'];
+    protected $fillable = ['user_id', 'verified_by', 'berita_category_id', 'berita_subcategory_id', 'title', 'description', 'image', 'is_verified'];
 
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+    public function verified_by()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
 }
