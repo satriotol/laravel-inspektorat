8a1a399a5ada92696308c4819afcb9f1c0f24fd4
bintoroadhi <bintoroadhisetiawan@gmail.com>
2022-11-11 08:17:16 +0700

    menambah kebijakan model controller

---
+++ app/Http/Controllers/KebijakanController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Kebijakan;
+use Illuminate\Http\Request;
+
+class KebijakanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Kebijakan  $kebijakan
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Kebijakan $kebijakan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Kebijakan  $kebijakan
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Kebijakan $kebijakan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Kebijakan  $kebijakan
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Kebijakan $kebijakan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Kebijakan  $kebijakan
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Kebijakan $kebijakan)
+    {
+        //
+    }
+}
