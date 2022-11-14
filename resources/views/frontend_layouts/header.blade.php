6a0d17b1386025ab03d8313503bb9995d24876a3
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2022-11-14 15:12:02 +0700

    frontend

---
+++ resources/views/frontend_layouts/header.blade.php
@@ -0,0 +1,186 @@
+<section class="top-bar-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-sm-12 col-xs-12">
+                <div class="top-info">
+                    <ul>
+                        <li><span class="icon-signs"></span>Discover St, New York, NY 10012, USA</li>
+                        <li><span class="icon-clock"></span>Opening Time: Mon-Sat: 09.00 to 18.00</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-8 col-xs-9">
+                <div class="top-right">
+                    <ul>
+                        <li><a href="#">Quick Links</a></li>
+                        <li><a href="#">Support</a></li>
+                        <li><a href="#">Register</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-1 col-sm-4 col-xs-3">
+                <div class="language-switcher">
+                    <div id="polyglotLanguageSwitcher">
+                        <form action="#">
+                            <select id="polyglot-language-options">
+                                <option id="en" value="en" selected>English</option>
+                                <option id="fr" value="fr">French</option>
+                                <option id="de" value="de">German</option>
+                                <option id="it" value="it">Italian</option>
+                                <option id="es" value="es">Spanish</option>
+                            </select>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+<header class="header-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-5 col-md-4 col-sm-12 col-xs-12">
+                <div class="logo">
+                    <a href="index.html">
+                        <img src="{{ asset('frontend_assets/images/resources/logo.png') }}" alt="Awesome Logo">
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-5 col-md-6 col-sm-12 col-xs-12">
+                <div class="header-contact-info">
+                    <ul>
+                        <li>
+                            <div class="iocn-holder">
+                                <span class="icon-technology"></span>
+                            </div>
+                            <div class="text-holder">
+                                <h6>Call Us On</h6>
+                                <p>1-8000-978-6543</p>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="iocn-holder">
+                                <span class="icon-letter top-envelop"></span>
+                            </div>
+                            <div class="text-holder">
+                                <h6>Mail Us @</h6>
+                                <p>Supportuss@gmail.com</p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-2 col-md-2 col-sm-12 col-xs-12">
+                <div class="top-social-links">
+                    <ul>
+                        <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                        <li><a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+                        <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                        <li><a href="#"><i class="fa fa-skype" aria-hidden="true"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</header>
+
+<section class="mainmenu-area stricky">
+    <div class="container">
+        <div class="mainmenu-bg">
+            <div class="row">
+                <div class="col-md-10 col-sm-12 col-xs-12">
+                    <!--Start mainmenu-->
+                    <nav class="main-menu">
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle" data-toggle="collapse"
+                                data-target=".navbar-collapse">
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                        </div>
+                        <div class="navbar-collapse collapse clearfix">
+                            <ul class="navigation clearfix">
+                                <li class="home current"><a href="index.html"><span class="fa fa-home"></span></a></li>
+                                <li class="dropdown"><a href="about.html">About us</a>
+                                    <ul>
+                                        <li><a href="company-overview.html">Company Overview</a></li>
+                                        <li><a href="company-history.html">Company History</a></li>
+                                        <li><a href="our-team.html">Our Team</a></li>
+                                        <li><a href="our-partner.html">Our Partner</a></li>
+                                        <li><a href="faq.html">FAQ's</a></li>
+                                        <li><a href="client-feedback.html">Client Feedback</a></li>
+                                        <li><a href="404.html">404 Page</a></li>
+                                    </ul>
+                                </li>
+                                <li class="dropdown"><a href="services.html">Our Services</a>
+                                    <ul>
+                                        <li><a href="business-planning.html">Business Planning</a></li>
+                                        <li><a href="audit-assurance.html">Audit & Assurance</a></li>
+                                        <li><a href="trades-stocks.html">Trades & Stocks</a></li>
+                                        <li><a href="investment-plan.html">Investment Plan</a></li>
+                                        <li><a href="risk-management.html">Risk Management</a></li>
+                                        <li><a href="succession-planning.html">Succession Planning</a></li>
+                                        <li><a href="strategic-planning.html">Strategic Planning</a></li>
+                                        <li><a href="financial-projections.html">Financial Projections</a></li>
+                                    </ul>
+                                </li>
+                                <li class="dropdown"><a href="blog.html">Latest News</a>
+                                    <ul>
+                                        <li><a href="blog.html">Blog Default</a></li>
+                                        <li><a href="blog-grid.html">Blog Grid View</a></li>
+                                        <li><a href="blog-list.html">Blog List View</a></li>
+                                        <li><a href="blog-single.html">Blog Single Post</a></li>
+                                    </ul>
+                                </li>
+                                <li class="dropdown"><a href="project.html">Projects</a>
+                                    <ul>
+                                        <li><a href="project-grid.html">Grid View</a></li>
+                                        <li><a href="project-grid-with-text.html">Grid With Text</a></li>
+                                        <li><a href="project.html">Projects Gallery Style</a></li>
+                                        <li><a href="project-single.html">Projects Single Post</a></li>
+                                    </ul>
+                                </li>
+                                <li class="dropdown"><a href="shop.html">Shop</a>
+                                    <ul>
+                                        <li><a href="shop.html">Shop Products</a></li>
+                                        <li><a href="shop-single.html">Single Product</a></li>
+                                        <li><a href="shopping-cart.html">Shopping Cart</a></li>
+                                        <li><a href="checkout.html">Checkout</a></li>
+                                        <li><a href="account.html">My Account</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="contact.html">Contact Us</a></li>
+                            </ul>
+                        </div>
+                    </nav>
+                    <!--End mainmenu-->
+                    <!--Start search box-->
+                    <div class="outer-search-box">
+                        <div class="seach-toggle"><i class="fa fa-search"></i></div>
+                        <ul class="search-box">
+                            <li>
+                                <form method="post" action="index.html">
+                                    <div class="form-group">
+                                        <input type="search" name="search" placeholder="Search Here" required>
+                                        <button type="submit"><i class="fa fa-search"></i></button>
+                                    </div>
+                                </form>
+                            </li>
+                        </ul>
+                    </div>
+                    <!--End search box-->
+                </div>
+                <div class="col-md-2 col-sm-12 col-xs-12">
+                    <div class="free-consulation-button pull-right">
+                        <a class="thm-btn bg-clr1" href="#">free Consultation</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
