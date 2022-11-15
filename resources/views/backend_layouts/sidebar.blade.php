a568f3784a32898b3fcde4f076dd8dfb3cecb97b
bintoroadhi <bintoroadhisetiawan@gmail.com>
2022-11-15 14:08:30 +0700

    menambah logo pemkot di backend

--- resources/views/backend_layouts/sidebar.blade.php
+++ resources/views/backend_layouts/sidebar.blade.php
@@ -3,13 +3,13 @@
     <div class="app-sidebar">
         <div class="side-header">
             <a class="header-brand1" href="{{ route('dashboard') }}">
-                <img src="{{ asset('backend_assets/images/brand/logo.png') }}" class="header-brand-img desktop-logo"
+                {{-- <img src="{{ asset('backend_assets/images/logoinspektorat.png') }}" class="header-brand-img desktop-logo"
                     alt="logo">
-                <img src="{{ asset('backend_assets/images/brand/logo-1.png') }}" class="header-brand-img toggle-logo"
+                <img src="{{ asset('backend_assets/images/pemkotsmg.png') }}" class="header-brand-img toggle-logo"
+                    alt="logo"> --}}
+                <img src="{{ asset('backend_assets/images/LambangSemarang.png') }}" class="header-brand-img light-logo"
                     alt="logo">
-                <img src="{{ asset('backend_assets/images/brand/logo-2.png') }}" class="header-brand-img light-logo"
-                    alt="logo">
-                <img src="{{ asset('backend_assets/images/brand/logo-3.png') }}" class="header-brand-img light-logo1"
+                <img src="{{ asset('backend_assets/images/logoinspektorat.png') }}" class="header-brand-img light-logo1"
                     alt="logo">
             </a>
             <!-- LOGO -->
