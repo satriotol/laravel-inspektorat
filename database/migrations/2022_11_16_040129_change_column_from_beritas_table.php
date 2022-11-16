19dd5ceb6c33dc6c730b801dbd8d27d59c0f3393
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2022-11-16 11:34:45 +0700

    change column

---
+++ database/migrations/2022_11_16_040129_change_column_from_beritas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('beritas', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id')->nullable()->change();
+            $table->unsignedBigInteger('verified_by')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('beritas', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id')->nullable(false)->change();
+            $table->unsignedBigInteger('verified_by')->nullable(false)->change();
+        });
+    }
+};
