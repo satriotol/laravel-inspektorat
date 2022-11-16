e5a9ffa6e41d44267ec3a50b0a29beb111269cea
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2022-11-16 09:19:30 +0700

    fixing

--- resources/views/backend/berita/index.blade.php
+++ resources/views/backend/berita/index.blade.php
@@ -25,7 +25,6 @@
                                 <tr>
                                     <th>Judul</th>
                                     <th>Kategori</th>
-                                    <th>Konten</th>
                                     <th>Gambar</th>
                                     <th>Action</th>
                                 </tr>
@@ -33,10 +32,17 @@
                             <tbody>
                                 @foreach ($beritas as $berita)
                                     <tr>
-                                        <td>{{ $berita->title }}</td>
-                                        <td>{{ $berita->berita_category->name }}</td>
-                                        <td>{!! $berita->description !!}</td>
-                                        <td><img src="{{ asset('uploads/' . $berita->image) }}" class="img-thumbnail" style="height: 100px" alt=""></td>
+                                        <td>
+                                            {{ $berita->title }} <br>
+                                            <div class="badge bg-primary">{{ $berita->user->name }}</div>
+                                        </td>
+                                        <td>
+                                            <a href="{{ route('beritaCategory.edit', $berita->berita_category_id) }}">
+                                                {{ $berita->berita_category->name }}
+                                            </a>
+                                        </td>
+                                        <td><img src="{{ asset('uploads/' . $berita->image) }}" class="img-thumbnail"
+                                                style="height: 100px" alt=""></td>
                                         <td>
                                             <form action="{{ route('berita.destroy', $berita->id) }}" method="post">
                                                 @csrf
