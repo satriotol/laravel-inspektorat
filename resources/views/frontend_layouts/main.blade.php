a7f67d47fec08b316543bc42d7412ffa0cc823d5
bintoroadhi <bintoroadhisetiawan@gmail.com>
2022-11-30 12:45:51 +0700

    profil ganti css dan js

--- resources/views/frontend_layouts/main.blade.php
+++ resources/views/frontend_layouts/main.blade.php
@@ -13,12 +13,7 @@
     <link rel="stylesheet" href="{{ asset('frontend_assets/css/style.css') }}">
     <!-- Responsive stylesheet -->
     <link rel="stylesheet" href="{{ asset('frontend_assets/css/responsive.css') }}">
-    <!-- Accordeon -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
-    </script>
+
     <!-- Favicon -->
     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('uploads/' . $master->logo) }}">
     <link rel="icon" type="image/png" href="{{ asset('uploads/' . $master->logo) }}" sizes="32x32">
@@ -299,11 +294,6 @@
         </script>
         <script src="{{ asset('frontend_assets/assets/revolution/js/extensions/revolution.extension.video.min.js') }}">
         </script>
-
-        <!-- accordeon js -->
-        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-
 
         <!-- thm custom script -->
         <script src="{{ asset('frontend_assets/js/custom.js') }}"></script>
