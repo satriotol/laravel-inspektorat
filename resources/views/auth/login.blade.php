8c340ba164c5dac4860829450b75de94698c7289
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2022-12-16 13:55:19 +0700

    otw user detail

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -93,6 +93,11 @@
                                             <button type="submit" class="login100-form-btn btn-primary"
                                                 @click="login">Masuk</button>
                                         </div>
+                                        <div class="text-center pt-3">
+                                            <p class="text-dark mb-0">Tidak Punya Akun ?<a
+                                                    href="{{ route('register') }}" class="text-primary ms-1">Daftar</a>
+                                            </p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
