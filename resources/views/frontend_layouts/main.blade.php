618ad9cb84eb45cce916eafa95f92f941390567b
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2022-12-19 14:19:17 +0700

    fixing

--- resources/views/frontend_layouts/main.blade.php
+++ resources/views/frontend_layouts/main.blade.php
@@ -301,7 +301,11 @@
         </script>
         @stack('custom-scripts')
         <script>
-            AOS.init();
+            AOS.init({
+                duration: 1000, // values from 0 to 3000, with step 50ms
+                easing: 'ease', // default easing for AOS animations
+                once: true, // whether animation should happen only once - while scrolling down
+            });
         </script>
 
 
