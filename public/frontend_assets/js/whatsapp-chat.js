520b37590f30196f8a6a261a94b567c285aca902
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2023-01-05 08:52:02 +0700

    fixing

--- public/frontend_assets/js/whatsapp-chat.js
+++ public/frontend_assets/js/whatsapp-chat.js
@@ -57,16 +57,16 @@ var whatsappchat = {
                 var chatBox = '<div class="ayoan_whatsapp_chatbox_container">' +
                     '<div class="rs-openChat" style="display: none"> <div class="ayoan_whatsapp_chatbox">' +
                     '<div class="widget-wrapper"> <div class="widget-header"> <div class="col col-1">' +
-                    '<img class="header-icon" src="https://sandbox.inspektorat.semarangkota.go.id/public/frontend_assets/images/icon/whatsapp.svg"> </div><div class="col col-2">' +
+                    '<img class="header-icon" src="https://inspektorat.semarangkota.go.id/public/frontend_assets/images/icon/whatsapp.svg"> </div><div class="col col-2">' +
                     '<div class="header-title">Layanan Inspektorat</div><div class="header-content">' + settings.headerMessage +
                     '</div></div></div><div class="widget-body"><div class="body-title">' + settings.chatBoxMessage +
                     '</div><div class="body-content ayoan_whatsapp_scroll1"><ul class="user-list">' + userList +
                     '</ul></div></div></div></div><div class="widget-close-btn-row" >' +
                     '<button type="button" class="rs-close widget-close-btn">' +
-                    '<img class="icon" src="https://sandbox.inspektorat.semarangkota.go.id/public/frontend_assets/images/icon/close.svg"></button></div></div>' +
+                    '<img class="icon" src="https://inspektorat.semarangkota.go.id/public/frontend_assets/images/icon/close.svg"></button></div></div>' +
                     '<div class="rs-openChatBtn widget-close-btn-row" > Butuh Bantuan Tekan Disini' +
                     '<button type="button" class="rs-openChatBtn widget-close-btn">' +
-                    '<img class="icon" src="https://sandbox.inspektorat.semarangkota.go.id/public/frontend_assets/images/icon/whatsapp.svg"></button></div></div>';
+                    '<img class="icon" src="https://inspektorat.semarangkota.go.id/public/frontend_assets/images/icon/whatsapp.svg"></button></div></div>';
                 element.innerHTML = chatBox;
                 changeColor(settings.color);
                 var goToWhatsapp = document.getElementsByClassName('rs-go-to-whatsapp');
